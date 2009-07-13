82cb7789e4dd74dbb5fe2b46d555493dbcf956c1
EmmanuelG <EmmanuelG@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-13 18:37:11 +0000

    Version 0.8.0
    git-svn-id: https://plugins.svn.wordpress.org/eg-delicious-sync/tags/0.8.0@134795 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,19 @@
+<?php
+
+ if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
+    exit();
+
+// --- Get options ---	
+define('EG_DELICIOUS_OPTIONS_ENTRY', 'EG-Delicious-Options');
+$eg_delicious_options = get_option(EG_DELICIOUS_OPTIONS_ENTRY);
+
+// --- Delete options (plugins and widgets ---
+if ($eg_delicious_options['uninstall_options']) {
+	delete_option(EG_DELICIOUS_OPTIONS_ENTRY);
+
+	if (get_option('widget_egdel_blogroll') !== FALSE) {
+		delete_option('widget_egdel_blogroll');
+	}
+}
+
+?>
